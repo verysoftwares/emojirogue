094ef63d0a51d929e801b462537af8d223c6cd16
verysoftwares <54413321+verysoftwares@users.noreply.github.com>
2022-03-09 15:31:15 +0200

    no extra fonts

--- load.lua
+++ load.lua
@@ -1,8 +1,4 @@
 function love.load()
-    -- emoji font source:
-    -- https://dn-works.com/ufas/
-    -- for personal use only
-    symbolfon= lg.newFont('wares/unifont-14.0.02.ttf', 64)
     emojifon= lg.newFont('wares/EmojiOneColor.otf', 64)
     emojifon2= lg.newFont('wares/TwitterColorEmoji-SVGinOT.ttf', 64)
     hoverfon= lg.newFont('wares/DaysOne-Regular.ttf', 64)
